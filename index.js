b6182f5c583c0dc924b842c84263495f1d07b732
Zakaria Talhami <zakaria.a.a.talhami@gmail.com>
2020-03-28 14:51:31 +0300

    feat: server tracks online user and updates the connected users

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ app.get("/", (req, res) => {
 // creates a socket object that can be used
 // to listen to events from user and emit messages back.
 io.on("connection", (socket) => {
-    console.log("A user connected");
-
     // get the nickname from query on socket initialization from client
     var nickname = socket.handshake.query.nickname;
+    console.log(`${nickname} has connected`);
 
     // Set and send users message color
     var messageColor = getRandomColor();
@@ -35,6 +34,9 @@ io.on("connection", (socket) => {
         messageColor: messageColor,
         messageCount: 0
     }
+
+    // Broadcast the online user list
+    broadcastOnlineUsers();
 
     // Broadcast when a new user enters the chat
     // this broadcasts the message to every other
@@ -60,7 +62,11 @@ io.on("connection", (socket) => {
 
     // Listen for the event of the socket getting disconnected
     socket.on("disconnect", () => {
-        console.log("A user has disconnected");
+        console.log(`${userDatabase[socket.id].nickname} has disconnected`);
+        delete userDatabase[socket.id];
+
+        // Broadcast the online user list
+        broadcastOnlineUsers();
     });
 
     // Listen for event of a user typing
@@ -74,6 +80,11 @@ http.listen(3000, () => {
     console.log("Listing on: localhost:3000");
 });
 
+// Broadcast the online user list
+// Excluding the server 
+var broadcastOnlineUsers = () => {
+    io.emit("online users", Object.values(userDatabase).filter((user) => user.nickname !== "Server"));
+};
 
 // Utils
 
